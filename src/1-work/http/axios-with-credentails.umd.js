8cde5653ab51b614378879869039cd2ad6d06b28
duanhejin <duanhejin@lanjingren.com>
2023-05-11 11:52:03 +0800

    feat: axios withCredentials demo

---
+++ src/1-work/http/axios-with-credentails.umd.js
@@ -0,0 +1,11 @@
+
+axios.defaults.withCredentials = true;
+
+async function testWithCredentials() {
+    const URL =
+        'https://img.epian1.com/migrate/mp_user/1041/mp_article/288972176/cover_d17d23ac169d3c621bd2c7bd69361965?imageMogr2/auto-orient|imageInfo';
+        
+    const { data } = await axios.get(URL, { withCredentials: false });
+    // const { data } = await axios.get(URL);
+    console.log('data', data)
+}
